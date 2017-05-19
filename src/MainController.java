4a7da69d898f5a77d778a0567dc419336014190a
Vlad <vlad.pythoner@gmail.com>
2017-05-19 19:54:26 +0300

    Обрисовал MainController

--- src/MainController.java
+++ src/MainController.java
@@ -1,5 +1,33 @@
+import java.util.List;
+
 /**
  * Created by Vlad on 19.05.2017.
  */
 public class MainController {
+
+    //Класс нужен, чтобы объединить данные пользователя, брони и отеля
+    //например, поиск броней по отелю и пользователю, удаление брони при уделении пользователя или комнаты
+    //и т.д.
+
+    UserController userController = new UserController();
+    HotelController hotelController = new HotelController();
+    BookingController bookingController = new BookingController();
+
+    public void createHotel(){}
+    public void deleteHotel(){}
+    public void updateHotel(){}
+    public void createRoom(){}
+    public void deleteRoom(){}
+    public void updateRoom(){}
+    public void createUser(){}
+    public void deleteUser(){}
+    public void updateUser(){}
+    public void createBooking(){}
+    public void deleteBooking(){}
+    public void updateBooking(){}
+    public List<Hotel> getHotelsByCity(){return null;}
+    public List<Hotel> getHotelsByName(){return null;}
+    public List<Booking> getBookingsByHotel(){return null;}
+    public List<Booking> getBookingsByRoom(){return null;}
+    public List<Booking> getBookingsByUser(){return null;}
 }
